6a403c2c64e69b8b585baa87826b109213d36e97
richaardev <me@richaar.dev>
2023-02-08 17:09:00 -0300

    fix: change to min-height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import "../styles/globals.css";
 
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col justify-center mx-auto max-w-5xl px-4 md:px-16">
+    <div className="flex min-h-screen flex-col justify-center mx-auto max-w-5xl px-4 md:px-16">
       <h1 className="text-md sm:text-2xl font-medium">Howdly!</h1>
       <h2 className="text-2xl sm:text-4xl font-bold">I'm richaardev</h2>
       <span className="text-sm sm:text-lg">
