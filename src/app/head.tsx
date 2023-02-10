feab0f9b8ce62e541cc5dc402c494684389e22fd
richaardev <me@richaar.dev>
2023-02-09 21:09:13 -0300

    feat: meta tags

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,6 +4,13 @@ export default function Head() {
       <title>richaardev's website</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+
+      <meta property="og:title" content="richaardev" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://richaar.dev" />
+      <meta property="og:description" content="Hi, i'm richaardev, a Full Stack developer and passionate about Technology!" />
+
+      <meta name="theme-color" content="#18181b" />
     </>
   );
 }
