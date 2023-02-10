2cd1ff5c321ad02891adf48519bb09c7f6d7ad47
richaardev <me@richaar.dev>
2023-02-09 21:13:22 -0300

    chore: change description

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -9,7 +9,7 @@ export default function Head() {
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://richaar.dev" />
       <meta property="og:image" content="https://cdn.discordapp.com/avatars/646416170123132959/a1fbb34ed5bca4847d3de96227ded664.png" />
-      <meta property="og:description" content="Hi, i'm richaardev, a Full Stack developer and passionate about Technology!" />
+      <meta property="og:description" content="Howdly! I'm richaardev, a full stack developer and passionate about Technology!" />
 
       <meta name="theme-color" content="#18181b" />
     </>
